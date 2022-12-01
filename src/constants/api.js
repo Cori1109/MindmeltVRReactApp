4b195661863cb48b252eeff46d397d52aed0a91d
richman225 <richman5225@gmail.com>
2022-12-01 11:50:04 -0500

    modified api url

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,1 @@
-// export const BASE_API_URL = "https://ab3104da-1df2-4e12-86fc-f210347fa43e.mock.pstmn.io/api/";
-export const BASE_API_URL = "http://mindmeltgoappvm.westeurope.cloudapp.azure.com/api";
+export const BASE_API_URL = "https://mindmeltgoappvm.westeurope.cloudapp.azure.com/api";
